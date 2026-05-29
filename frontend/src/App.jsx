42b6895d00623ced3d95e0af05ece914276a0948
micherenio <micherenio.nascimento@gmail.com>
2026-05-29 15:06:47 -0300

    Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import './App.css'
+import alytechLogo from './assets/alytech-logo.svg'
 
 function App() {
   return (
-    <main className="dashboard-shell">
-      <iframe
-        className="dashboard-frame"
-        src="http://18.191.242.141:3001/d/dse855-scada/dse-855-scada?orgId=1&from=now-5m&to=now&timezone=browser&refresh=5s&kiosk"
-        title="Grafana Dashboard"
-      />
-    </main>
+    <div className="app-shell">
+      <header className="app-header">
+        <img className="app-logo" src={alytechLogo} alt="ALYTECH Solucoes e Servicos" />
+      </header>
+
+      <main className="dashboard-shell">
+        <iframe
+          className="dashboard-frame"
+          src="http://18.191.242.141:3001/d/dse855-scada/dse-855-scada?orgId=1&from=now-5m&to=now&timezone=browser&refresh=5s&kiosk"
+          title="Grafana Dashboard"
+        />
+      </main>
+    </div>
   );
 }
 
